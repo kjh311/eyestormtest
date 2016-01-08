1f8079ae4fdfa74723a9af2bb79def405c5205f3
Kevin Huelsmann <kjh311@gmail.com>
2016-01-07 18:29:41 -0800

    progress bar works

--- script/javascript.js
+++ script/javascript.js
@@ -1,3 +1,166 @@
 $( document ).ready(function() {
-    // alert( "ready, mo fo!" );
+
+  $(function() {
+    var clicks = 0;
+    $('.icon').on('click', function() {
+    clicks++;
+    var percent = Math.min(Math.round(clicks / 10 * 100), 100);
+    $('.progress-bar').width(percent + '%').text(percent + '%');
+    });
+  });
+
+  $( "#baggins-icon" ).click(function() {
+    width = $(".progress-bar").css("width");
+    $("#baggins-pic").addClass( "picVisible" );
+    $("#baggins-icon").addClass( "iconLowerOpacity" );
+    $("#corleone-pic").removeClass( "picVisible" );
+    $("#mcdonalds-pic").removeClass( "picVisible" );
+    $("#microsoft-pic").removeClass( "picVisible" );
+    $("#parker-pic").removeClass( "picVisible" );
+    $("#potter-pic").removeClass( "picVisible" );
+    $("#skywalker-pic").removeClass( "picVisible" );
+    $("#sparrow-pic").removeClass( "picVisible" );
+    $("#spock-pic").removeClass( "picVisible" );
+    $("#wayne-pic").removeClass( "picVisible" );
+    $("#baggins-icon").addClass( "iconHidden" );
+  });
+
+  $( "#corleone-icon" ).click(function() {
+    width = $(".progress-bar").css("width");
+    $("#corleone-pic").addClass( "picVisible" );
+    $( "#corleone-icon" ).addClass( "iconLowerOpacity" );
+    $("#baggins-pic").removeClass( "picVisible" );
+    $("#mcdonalds-pic").removeClass( "picVisible" );
+    $("#microsoft-pic").removeClass( "picVisible" );
+    $("#parker-pic").removeClass( "picVisible" );
+    $("#potter-pic").removeClass( "picVisible" );
+    $("#skywalker-pic").removeClass( "picVisible" );
+    $("#sparrow-pic").removeClass( "picVisible" );
+    $("#spock-pic").removeClass( "picVisible" );
+    $("#wayne-pic").removeClass( "picVisible" );
+    $("#corleone-icon").addClass( "iconHidden" );
+  });
+
+  $( "#mcdonalds-icon" ).click(function() {
+    $("#mcdonalds-pic").addClass( "picVisible" );
+    $( "#mcdonalds-icon" ).addClass( "iconLowerOpacity" );
+    $("#baggins-pic").removeClass( "picVisible" );
+    $("#corleone-pic").removeClass( "picVisible" );
+    $("#microsoft-pic").removeClass( "picVisible" );
+    $("#parker-pic").removeClass( "picVisible" );
+    $("#potter-pic").removeClass( "picVisible" );
+    $("#skywalker-pic").removeClass( "picVisible" );
+    $("#sparrow-pic").removeClass( "picVisible" );
+    $("#spock-pic").removeClass( "picVisible" );
+    $("#wayne-pic").removeClass( "picVisible" );
+    $("#mcdonalds-icon").addClass( "iconHidden" );
+  });
+
+  $( "#microsoft-icon" ).click(function() {
+    $("#microsoft-pic").addClass( "picVisible" );
+    $( "#microsoft-icon" ).addClass( "iconLowerOpacity" );
+    $("#baggins-pic").removeClass( "picVisible" );
+    $("#corleone-pic").removeClass( "picVisible" );
+    $("#mcdonalds-pic").removeClass( "picVisible" );
+    $("#parker-pic").removeClass( "picVisible" );
+    $("#potter-pic").removeClass( "picVisible" );
+    $("#skywalker-pic").removeClass( "picVisible" );
+    $("#sparrow-pic").removeClass( "picVisible" );
+    $("#spock-pic").removeClass( "picVisible" );
+    $("#wayne-pic").removeClass( "picVisible" );
+    $("#microsoft-icon").addClass( "iconHidden" );
+  });
+
+  $( "#parker-icon" ).click(function() {
+    $("#parker-pic").addClass( "picVisible" );
+    $( "#parker-icon" ).addClass( "iconLowerOpacity" );
+    $("#baggins-pic").removeClass( "picVisible" );
+    $("#corleone-pic").removeClass( "picVisible" );
+    $("#mcdonalds-pic").removeClass( "picVisible" );
+    $("#microsoft-pic").removeClass( "picVisible" );
+    $("#potter-pic").removeClass( "picVisible" );
+    $("#skywalker-pic").removeClass( "picVisible" );
+    $("#sparrow-pic").removeClass( "picVisible" );
+    $("#spock-pic").removeClass( "picVisible" );
+    $("#wayne-pic").removeClass( "picVisible" );
+    $("#parker-icon").addClass( "iconHidden" );
+  });
+
+  $( "#potter-icon" ).click(function() {
+    $("#potter-pic").addClass( "picVisible" );
+    $( "#potter-icon" ).addClass( "iconLowerOpacity" );
+    $("#baggins-pic").removeClass( "picVisible" );
+    $("#corleone-pic").removeClass( "picVisible" );
+    $("#mcdonalds-pic").removeClass( "picVisible" );
+    $("#microsoft-pic").removeClass( "picVisible" );
+    $("#parker-pic").removeClass( "picVisible" );
+    $("#skywalker-pic").removeClass( "picVisible" );
+    $("#sparrow-pic").removeClass( "picVisible" );
+    $("#spock-pic").removeClass( "picVisible" );
+    $("#wayne-pic").removeClass( "picVisible" );
+    $("#potter-icon").addClass( "iconHidden" );
+  });
+
+  $( "#skywalker-icon" ).click(function() {
+    $("#skywalker-pic").addClass( "picVisible" );
+    $( "#skywalker-icon" ).addClass( "iconLowerOpacity" );
+    $("#baggins-pic").removeClass( "picVisible" );
+    $("#corleone-pic").removeClass( "picVisible" );
+    $("#mcdonalds-pic").removeClass( "picVisible" );
+    $("#microsoft-pic").removeClass( "picVisible" );
+    $("#parker-pic").removeClass( "picVisible" );
+    $("#potter-pic").removeClass( "picVisible" );
+    $("#sparrow-pic").removeClass( "picVisible" );
+    $("#spock-pic").removeClass( "picVisible" );
+    $("#wayne-pic").removeClass( "picVisible" );
+    $("#skywalker-icon").addClass( "iconHidden" );
+  });
+
+  $( "#sparrow-icon" ).click(function() {
+    $("#sparrow-pic").addClass( "picVisible" );
+    $("#sparrow-icon" ).addClass( "iconLowerOpacity" );
+    $("#baggins-pic").removeClass( "picVisible" );
+    $("#corleone-pic").removeClass( "picVisible" );
+    $("#mcdonalds-pic").removeClass( "picVisible" );
+    $("#microsoft-pic").removeClass( "picVisible" );
+    $("#parker-pic").removeClass( "picVisible" );
+    $("#potter-pic").removeClass( "picVisible" );
+    $("#skywalker-pic").removeClass( "picVisible" );
+    $("#spock-pic").removeClass( "picVisible" );
+    $("#wayne-pic").removeClass( "picVisible" );
+    $("#sparrow-icon").addClass( "iconHidden" );
+  });
+
+  $( "#spock-icon" ).click(function() {
+    $("#spock-pic").addClass( "picVisible" );
+    $("#spock-icon" ).addClass( "iconLowerOpacity" );
+    $("#baggins-pic").removeClass( "picVisible" );
+    $("#corleone-pic").removeClass( "picVisible" );
+    $("#mcdonalds-pic").removeClass( "picVisible" );
+    $("#microsoft-pic").removeClass( "picVisible" );
+    $("#parker-pic").removeClass( "picVisible" );
+    $("#potter-pic").removeClass( "picVisible" );
+    $("#skywalker-pic").removeClass( "picVisible" );
+    $("#sparrow-pic").removeClass( "picVisible" );
+    $("#wayne-pic").removeClass( "picVisible" );
+    $("#spock-icon").addClass( "iconHidden" );
+  });
+
+  $( "#wayne-icon" ).click(function() {
+    $("#wayne-pic").addClass( "picVisible" );
+    $("#wayne-icon" ).addClass( "iconLowerOpacity" );
+    $("#baggins-pic").removeClass( "picVisible" );
+    $("#corleone-pic").removeClass( "picVisible" );
+    $("#mcdonalds-pic").removeClass( "picVisible" );
+    $("#microsoft-pic").removeClass( "picVisible" );
+    $("#parker-pic").removeClass( "picVisible" );
+    $("#potter-pic").removeClass( "picVisible" );
+    $("#skywalker-pic").removeClass( "picVisible" );
+    $("#sparrow-pic").removeClass( "picVisible" );
+    $("#spock-pic").removeClass( "picVisible" );
+    $("#wayne-icon").addClass( "iconHidden" );
+  });
+
+
+
 });
